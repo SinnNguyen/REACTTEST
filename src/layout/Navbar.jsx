0fffe95a6b30f4e0c8a01afe48f028dc33efa3b6
GS-755 <21dh114236@st.huflit.edu.vn>
2023-11-16 06:27:00 +0700

    Complete blue navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,123 @@
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const Navbar = () => {
   return (
-    <>
-      <p>adu vip :)))</p>
-    </>
+    <Disclosure as="nav" className="bg-blue-400">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Hồ sơ cá nhân</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <h3 className='font-bold text-2xl text-white'>HYPER TECH</h3>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    <form class="w-full max-w-sm">
+                      <div class="flex items-center border-b border-teal-500 px-10">
+                        <input class="shadow appearance-none border rounded w-full py-2 mt-3 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name='searchBar' type="text" placeholder="Tìm kiếm sản phẩm"></input>
+                        <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 mx-3 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                          Tìm kiếm
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="relative rounded-full p-1 text-white focus:outline-none"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Xem thông báo</span>
+                  <ShoppingCartIcon className="h-6 w-6 mt-2" aria-hidden="true" /><sup>(1)</sup>
+                </button>
+                <p className='font-semibold mx-5 my-5'>Nhựt Trường</p>
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Bật menu người dùng</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://cdn.icon-icons.com/icons2/2468/PNG/512/user_icon_149329.png"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Thông tin cá nhân
+                          </a>
+                        )}
+                      </Menu.Item>
+
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Cài đặt
+                          </a>
+                        )}
+                      </Menu.Item>
+
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Đăng xuất
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
   );
 }
 
